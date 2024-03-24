c3326abc24f5f307cf5afe38839346cef27c469f
ebissa <ebbissa.chemeda@gmail.com>
2024-03-24 23:28:37 +0300

    modified

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import headerImg from "../assets/img/header-img.svg";
 import AstronountImg from "../assets/img/astrounount.png";
 
 import { ArrowRightCircle } from "react-bootstrap-icons";
