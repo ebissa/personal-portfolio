07d662b60456f37becb07d5babc692b25f73405c
ebissa <ebbissa.chemeda@gmail.com>
2024-03-25 09:37:27 +0300

    modified

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -11,7 +11,7 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  // const [index, setIndex] = useState(1);
+  const [index, setIndex] = useState(1);
   const toRotate = ["Full Stack Developer", "UI/UX Designer", "Freelancer"];
   const period = 2000;
 
@@ -49,6 +49,7 @@ export const Banner = () => {
       setDelta(500);
     } else {
       setIndex((prevIndex) => prevIndex + 1);
+      console.log(index);
     }
   };
 
